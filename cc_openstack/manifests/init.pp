4fcc8032891643a2054d0f05750050a03e03a9ef
tobias <tobias.schaber@codecentric.de>
2014-11-20 12:01:08 +0100

    added hosts

--- cc_openstack/manifests/init.pp
+++ cc_openstack/manifests/init.pp
@@ -3,7 +3,6 @@ class cc_openstack {
 
 	include cc_openstack::roles::controller_node
 
-
 }
 
 include cc_openstack
--- cc_openstack/manifests/roles/controller_node/network.pp
+++ cc_openstack/manifests/roles/controller_node/network.pp
@@ -7,7 +7,11 @@ class cc_openstack::roles::controller_node::network {
 	File_Line['append_mgmt_interface_2'] ->
 	File_Line['append_mgmt_interface_3'] ->
 	File_Line['append_mgmt_interface_4'] ->
-	File_Line['append_mgmt_interface_5']
+	File_Line['append_mgmt_interface_5'] ->
+	Exec['ifup_eth1'] ->
+	File_Line['etc_hosts_change_1'] ->
+	File_Line['etc_hosts_change_2'] ->
+	File_Line['etc_hosts_change_3']
 	
 
 	file_line { 'append_mgmt_interface_1':
@@ -33,6 +37,28 @@ class cc_openstack::roles::controller_node::network {
 	file_line { 'append_mgmt_interface_5':
 		path => '/etc/network/interfaces',
 		line => '	gateway 10.0.0.1',
+	}
+	
+	exec { "ifup_eth1":
+       command => "ifup eth1",
+       path    => "/usr/bin",
+	}
+	
+
+	file_line { 'etc_hosts_change_1':
+		path	=> '/etc/hosts',
+		match	=> '127.0.1.1	controller',
+		line	=> '#127.0.1.1	controller',
+	}
+	
+	file_line { 'etc_hosts_change_2':
+		path	=> '/etc/hosts',
+		line	=> '# controller',
+	}
+	
+	file_line { 'etc_hosts_change_3':
+		path	=> '/etc/hosts',
+		line	=> '10.0.0.11	controller',
 	}
 
 
