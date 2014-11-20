73f17d6b9ec5a5e7841b3ab875638639327895e7
tobias <tobias.schaber@codecentric.de>
2014-11-20 12:52:30 +0100

    mysql

--- cc_openstack/manifests/roles/controller_node.pp
+++ cc_openstack/manifests/roles/controller_node.pp
@@ -22,17 +22,26 @@ class cc_openstack::roles::controller_node {
 		ensure => "installed",
 	}
 	
-	exec { 'install-mysql-server':
-		command => "apt-get install -y mysql-server",
-		path    => [ "/bin", "/usr/bin", "/sbin", "/usr/local/sbin", "/usr/sbin"],
-	}
+#	exec { 'install-mysql-server':
+#		command => "apt-get install -y mysql-server",
+#		path    => [ "/bin", "/usr/bin", "/sbin", "/usr/local/sbin", "/usr/sbin"],
+#	}
 	
-	file_line { 'setup_mysql_server_1' :
-		path	=> '/etc/mysql/my.cnf',
-		match	=> 'bind-address		= 127.0.0.1',
-		line	=> 'bind-address		= 10.0.0.11\nappendhere',
+#	file_line { 'setup_mysql_server_1' :
+#		path	=> '/etc/mysql/my.cnf',
+#		match	=> 'bind-address		= 127.0.0.1',
+#		line	=> 'bind-address		= 10.0.0.11',
+#	}
+
+	class { '::mysql::server':
+		root_password   => 'tobias1234',
+		override_options => { 
+
+		  # listen on external address
+		  'mysqld' => { 'bind-address' => '10.0.1.11' } , 
+		}
 	}
-	
+
 	
 	
 	
