106f9b3cfb027df907ea20a6dd717af14195966c
tobias <tobias.schaber@codecentric.de>
2014-11-20 12:13:45 +0100

    added ntp

--- cc_openstack/manifests/roles/controller_node.pp
+++ cc_openstack/manifests/roles/controller_node.pp
@@ -3,7 +3,12 @@
 
 class cc_openstack::roles::controller_node {
 
-	include cc_openstack::roles::controller_node::network
+	#include cc_openstack::roles::controller_node::network
+	
+	package { 'ntp':
+		ensure	=> "installed",
+		#require	=> Exec['apt-update'],
+	}
 	
 	
 	
