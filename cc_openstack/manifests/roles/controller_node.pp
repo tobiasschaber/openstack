b1d323f72eb8652a13de26efcfdb96e03e0a8aa1
tobias <tobias.schaber@codecentric.de>
2014-11-20 13:08:13 +0100

    mysdql

--- cc_openstack/manifests/roles/controller_node.pp
+++ cc_openstack/manifests/roles/controller_node.pp
@@ -7,8 +7,7 @@ class cc_openstack::roles::controller_node {
 	
 	Package['ntp'] ->
 	Package['python-mysqldb'] ->
-	Exec['install-mysql-server'] ->
-	File_Line['setup_mysql_server_1']
+	Class{'::mysql::server']
 	
 
 	
@@ -21,20 +20,11 @@ class cc_openstack::roles::controller_node {
 	package { 'python-mysqldb':
 		ensure => "installed",
 	}
-	
-#	exec { 'install-mysql-server':
-#		command => "apt-get install -y mysql-server",
-#		path    => [ "/bin", "/usr/bin", "/sbin", "/usr/local/sbin", "/usr/sbin"],
-#	}
-	
-#	file_line { 'setup_mysql_server_1' :
-#		path	=> '/etc/mysql/my.cnf',
-#		match	=> 'bind-address		= 127.0.0.1',
-#		line	=> 'bind-address		= 10.0.0.11',
-#	}
 
 	class { '::mysql::server':
-		root_password   => 'tobias1234',
+		root_password   		=> 'tobias1234',
+		remove_default_accounts => 'true',
+		restart					=> 'true',
 		override_options => { 
 
 			'mysqld' => { 
