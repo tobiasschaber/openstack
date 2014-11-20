f39c70026ca49247ba623c67cf358e7cf27ae91c
tobias <tobias.schaber@codecentric.de>
2014-11-20 12:27:28 +0100

    change

--- cc_openstack/manifests/roles/controller_node.pp
+++ cc_openstack/manifests/roles/controller_node.pp
@@ -3,13 +3,34 @@
 
 class cc_openstack::roles::controller_node {
 
-	#include cc_openstack::roles::controller_node::network
+	include cc_openstack::roles::controller_node::network
+	
+	Package['ntp'] ->
+	Package['python-mysqldb'] ->
+	Exec['install-mysql-server']
+	
+
+	
 	
 	package { 'ntp':
 		ensure	=> "installed",
 		#require	=> Exec['apt-update'],
 	}
 	
+	package { 'python-mysqldb':
+		ensure => "installed",
+	}
+	
+	exec { 'install-mysql-server':
+	
+		command => "apt-get install -y mysql-server",
+        cwd     => "/var/lib/tftpboot/boot/",
+        creates => "/var/lib/tftpboot/boot/foreman-discovery-image-latest.el6.iso-vmlinuz",
+        path    => "/usr/bin",
+        timeout => 1000,
+        require => File["/var/lib/tftpboot/boot"],
+	
+	}
 	
 	
 	
