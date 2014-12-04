6c9ff90cd476a4d61da2b8e001ed67ff7c789f3a
tobias <tobias.schaber@codecentric.de>
2014-12-04 10:50:27 +0100

    changed client instsall from pip to apt

--- cc_openstack/manifests/roles/controller_node/clienttools.pp
+++ cc_openstack/manifests/roles/controller_node/clienttools.pp
@@ -4,15 +4,15 @@
 class cc_openstack::roles::controller_node::clienttools {
 
 	Exec['install_pip'] ->
-	Package['install_client_ceilometer'] ->
-	Exec['install_client_cinder'] ->
-	Exec['install_client_glance'] ->
-	Exec['install_client_heat'] ->
-	Exec['install_client_keystone'] ->
-	Exec['install_client_neutron'] ->
-	Exec['install_client_nova'] ->
-	Exec['install_client_swift'] ->
-	Exec['install_client_trove']
+	Package['python-ceilometerclient'] ->
+	Package['python-cinderclient'] ->
+	Package['python-glanceclient'] ->
+	Package['python-heatclient'] ->
+	Package['python-keystoneclient'] ->
+	Package['python-neutronclient'] ->
+	Package['python-novaclient'] ->
+	Package['python-swiftclient'] ->
+	Package['python-troveclient']
 	
 	
 	exec { 'install_pip':
@@ -24,48 +24,38 @@ class cc_openstack::roles::controller_node::clienttools {
 		ensure => "installed",
 	}
 	
-#	exec { 'install_client_ceilometer':
-#		command => 'pip install python-ceilometerclient',
-#		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin', '/usr/local/bin/'],
-#	}
-	
-	exec { 'install_client_cinder':
-		command => 'pip install python-cinderclient',
-		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin', '/usr/local/bin/'],
+	package { 'python-cinderclient':
+		ensure => "installed",
 	}
 	
-	exec { 'install_client_glance':
-		command => 'pip install python-glanceclient',
-		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin', '/usr/local/bin/'],
+	package { 'python-glanceclient':
+		ensure => "installed",
 	}
 	
-	exec { 'install_client_heat':
-		command => 'pip install python-heatclient',
-		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin', '/usr/local/bin/'],
+	package { 'python-heatclient':
+		ensure => "installed",
 	}
 	
-	exec { 'install_client_keystone':
-		command => 'pip install python-keystoneclient',
-		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin', '/usr/local/bin/'],
+	package { 'python-keystoneclient':
+		ensure => "installed",
 	}
 	
-	exec { 'install_client_neutron':
-		command => 'pip install python-neutronclient',
-		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin', '/usr/local/bin/'],
+	package { 'python-neutronclient':
+		ensure => "installed",
 	}
 	
-	exec { 'install_client_nova':
-		command => 'pip install python-novaclient',
-		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin', '/usr/local/bin/'],
+	package { 'python-novaclient':
+		ensure => "installed",
 	}
 	
-	exec { 'install_client_swift':
-		command => 'pip install python-swiftclient',
-		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin', '/usr/local/bin/'],
+	package { 'python-swiftclient':
+		ensure => "installed",
 	}
 	
-	exec { 'install_client_trove':
-		command => 'pip install python-troveclient',
-		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin', '/usr/local/bin/'],
+	package { 'python-troveclient':
+		ensure => "installed",
 	}
+	
+	
+	
 }
