56e96aba6c48f627128b37efac607d1fd9d56a5b
tobias <tobias.schaber@codecentric.de>
2014-12-09 14:15:36 +0100

    nova client

--- cc_openstack/manifests/roles/compute_node/nova.pp
+++ cc_openstack/manifests/roles/compute_node/nova.pp
@@ -4,11 +4,20 @@
 
 class cc_openstack::roles::compute_node::nova {
 
+	Exec['perform-apt-get-update'] ->	
 	Package['nova-compute-kvm']
+	
+	
+
+	exec { 'perform-apt-get-update':
+		command => 'apt-get update',
+		path => ['/usr/bin/', '/bin/', '/sbin/', '/usr/sbin'],
+	}
+
 
 	package { 'nova-compute-kvm':
 		ensure => "installed",
 	}
-	
+		
 
 }
